2cea8224413c863b81a6d9f7f909f33c2dac7841
Tsurkanovskiy <sergius.tsurkanovskiy@gmail.com>
2023-02-17 20:23:18 +0200

    Create basic views to read data

--- studyingorganizerapp/views.py
+++ studyingorganizerapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Semester, Subject, Assignment
 
 # Create your views here.
+def home(request):
+	semesters = Semester.objects.all()
+	return render(request, "home.html", {"semesters": semesters})
+
+# View semester and all subjects in it
+def semester(request, semester_id):
+	current_semester = Semester.objects.get(id = semester_id)
+	return render(request, "semester.html", {"semester": current_semester})
+
+# View subject and all assignments in it
+def subject(request, subject_id):
+	current_subject = Semester.objects.get(id = subject_id)
+	return render(request, "subject.html", {"subject": current_subject})
