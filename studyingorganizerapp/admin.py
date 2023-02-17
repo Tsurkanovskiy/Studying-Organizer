6837e181a3695584b872b0aa76bf31bd6f36196f
Tsurkanovskiy <sergius.tsurkanovskiy@gmail.com>
2023-02-17 22:10:15 +0200

    Registered models for admin panel

--- studyingorganizerapp/admin.py
+++ studyingorganizerapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Semester, Subject, Assignment
 
 # Register your models here.
+admin.site.register(Semester)
+admin.site.register(Subject)
+admin.site.register(Assignment)
