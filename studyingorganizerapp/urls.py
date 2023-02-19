30c1937f13bf7350c558ccad9aa2dfb9fa346f7f
Tsurkanovskiy <sergius.tsurkanovskiy@gmail.com>
2023-02-20 01:13:46 +0200

    Added deadlines page to view all assignments sorted by their deadline

--- studyingorganizerapp/urls.py
+++ studyingorganizerapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name = "home"),
     path('semester/<int:semester_id>/', views.semester, name = "semester"),
     path('subject/<int:subject_id>/', views.subject, name = "subject"),
+    path('semester/<int:semester_id>/deadlines', views.deadlines, name = "deadlines"),
 ]
--- studyingorganizerapp/views.py
+++ studyingorganizerapp/views.py
@@ -11,8 +11,23 @@ def home(request):
 def semester(request, semester_id):
 	current_semester = Semester.objects.get(id = semester_id)
 	return render(request, "semester.html", {"semester": current_semester})
+	#for future CRUD functionality
+	#HttpResponseRedirect("/semester/{0}".format(semester_id))
 
 # View subject and all assignments in it
 def subject(request, subject_id):
 	current_subject = Subject.objects.get(id = subject_id)
 	return render(request, "subject.html", {"subject": current_subject})
+
+# View all deadlines in curent semester
+def deadlines(request, semester_id):
+	current_semester = Semester.objects.get(id = semester_id)
+	#access several levels foreign key relationships
+	assignments = Assignment.objects.filter(subject__semester=current_semester).order_by('deadline')
+
+	#get course/semester completion score
+	open_assignments = assignments.filter(is_open=True)
+	completion_score = round(len(open_assignments.filter(is_finished=True))/len(open_assignments), 2)*100
+
+
+	return render(request, "deadlines.html", {"assignments": assignments, "completion_score": completion_score})
